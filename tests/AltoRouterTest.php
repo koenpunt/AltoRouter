d5a38ce0bc01cf8edc99bb7865377b5100f33fca
Koen Punt <koen@koenpunt.nl>
2015-03-01 22:30:44 +0100

    add test for request helper methods

--- tests/AltoRouterTest.php
+++ tests/AltoRouterTest.php
@@ -291,14 +291,21 @@ class AltoRouterTest extends PHPUnit_Framework_TestCase
 		$this->assertFalse($this->router->match('/users/abc/delete', 'POST'));
 		$this->assertFalse($this->router->match('/users/1/create', 'GET'));
 	}
-	
-	public function testMatchWithServerVars()
+
+	public function testMatchWithRequest()
 	{
-		$this->router->map('GET', '/foo/[:controller]/[:action]', 'foo_action', 'foo_route');
-		
-		$_SERVER['REQUEST_URI'] = '/foo/test/do';
-		$_SERVER['REQUEST_METHOD'] = 'GET';
-		
+		$router = $this->getMockBuilder('AltoRouterDebug')
+			->setMethods(array('getRequestURI', 'getRequestMethod'))
+			->getMock();
+
+		$router->method('getRequestURI')
+			->willReturn('/foo/test/do');
+
+		$router->method('getRequestMethod')
+			->willReturn('POST');
+
+		$router->map('POST', '/foo/[:controller]/[:action]', 'foo_action', 'foo_route');
+
 		$this->assertEquals(array(
 			'target' => 'foo_action',
 			'params' => array(
@@ -306,7 +313,7 @@ class AltoRouterTest extends PHPUnit_Framework_TestCase
 				'action' => 'do'
 			),
 			'name' => 'foo_route'
-		), $this->router->match());
+		), $router->match());
 	}
 	
 	public function testMatchWithOptionalUrlParts()
@@ -430,4 +437,26 @@ class AltoRouterTest extends PHPUnit_Framework_TestCase
 		
 		$this->assertFalse($this->router->match('/﷽‎', 'GET'));
 	}
+
+	/**
+	 * @covers AltoRouter::getRequestMethod
+	 * @covers AltoRouter::getRequestURI
+	 */
+	public function testMatchWithRequestFromServer()
+	{
+		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_SERVER['REQUEST_URI'] = '/foo/test/do';
+
+		$this->router->map('POST', '/foo/[:controller]/[:action]', 'foo_action', 'foo_route');
+
+		$this->assertEquals(array(
+			'target' => 'foo_action',
+			'params' => array(
+				'controller' => 'test',
+				'action' => 'do'
+			),
+			'name' => 'foo_route'
+		), $this->router->match());
+	}
+
 }
